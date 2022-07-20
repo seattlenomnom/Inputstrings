9ca7292d39e9d900699d00bcdd947ad1314535b2
seattlenomnom <mark.crapser@gmail.com>
2022-07-19 17:00:12 -0700

    added input screen and scanf, modded README

--- inputstrings.c
+++ inputstrings.c
@@ -2,6 +2,10 @@
  * source file for inputstrings, a program that introduces scanf for %s strings
  * and will experiment using \t and \v to create an input page.
  *
+ * \v and \t works, \a does not work. An input screen not using ncurses?
+ *
+ * Done.
+ *
 */
 
 #include <stdio.h>
@@ -14,22 +18,28 @@
 int main(int argc, char *argv[]) {
 
 
+    char s1[81];
+
+    /* input */
+    /* create an input screen */
+
+    printf("\v");
+    printf("\t");
+    printf("\a");
+    printf("Input a string: ");
+    scanf("%80s", s1);
+    printf("\n");
 
 
 
 
 
+    /* process */
 
 
 
-
-
-
-
-
-
-
-
+    /* output */
+    printf("The string is %s\n", s1);
 
 
 
